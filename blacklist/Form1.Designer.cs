5d41ceb59fa3ce751768e7c1033c80e5f0db852f
CaranTsui <carantsui@gmail.com>
2016-10-22 15:42:43 +0800

    commit all codes

---
+++ blacklist/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace blacklist
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.server = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.username = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.reason = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.info = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(534, 245);
+            this.panel1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.server,
+            this.username,
+            this.reason,
+            this.info});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(534, 245);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // id
+            // 
+            this.id.HeaderText = "id";
+            this.id.Name = "id";
+            this.id.Visible = false;
+            // 
+            // server
+            // 
+            this.server.HeaderText = "服务器";
+            this.server.Name = "server";
+            // 
+            // username
+            // 
+            this.username.HeaderText = "ID";
+            this.username.Name = "username";
+            // 
+            // reason
+            // 
+            this.reason.HeaderText = "黑历史";
+            this.reason.Name = "reason";
+            this.reason.Width = 200;
+            // 
+            // info
+            // 
+            this.info.HeaderText = "故事";
+            this.info.Name = "info";
+            this.info.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(432, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "加入黑名单";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(14, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "搜索";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.richTextBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.comboBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.comboBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.panel1);
+            this.splitContainer1.Size = new System.Drawing.Size(534, 412);
+            this.splitContainer1.SplitterDistance = 163;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(304, 60);
+            this.richTextBox1.MaxLength = 128;
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedHorizontal;
+            this.richTextBox1.Size = new System.Drawing.Size(203, 56);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(216, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "说出你的故事";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(69, 56);
+            this.textBox1.MaxLength = 128;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "黑历史";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "划划水",
+            "黑装备",
+            "脾气差",
+            "骂脏话"});
+            this.comboBox2.Location = new System.Drawing.Point(304, 31);
+            this.comboBox2.MaxLength = 128;
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(96, 20);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "游戏ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "服务器";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "拉诺西亚",
+            "紫水栈桥",
+            "幻影群岛",
+            "魔杜纳",
+            "神意之地",
+            "静语庄园",
+            "萌芽池"});
+            this.comboBox1.Location = new System.Drawing.Point(68, 31);
+            this.comboBox1.MaxLength = 128;
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(101, 20);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 412);
+            this.Controls.Add(this.splitContainer1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(550, 450);
+            this.MinimumSize = new System.Drawing.Size(550, 450);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FF14简易黑名单";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn server;
+        private System.Windows.Forms.DataGridViewTextBoxColumn username;
+        private System.Windows.Forms.DataGridViewTextBoxColumn reason;
+        private System.Windows.Forms.DataGridViewTextBoxColumn info;
+
+    }
+}
+
---
+++ blacklist/Form1.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Data;
+using System.Data.Entity;
+
+namespace blacklist
+{
+
+    public partial class Form1 : Form
+    {
+        private db dbInstance = new db();        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string server = this.comboBox1.Text;
+            string username = this.textBox1.Text;
+            string reason = this.comboBox2.Text;
+            string info = this.richTextBox1.Text;
+            if (server == "" && username == "")
+            {
+                MessageBox.Show("服务器和ID请至少输入一个。");
+                return;
+            }
+
+            if (reason == "")
+            {
+                MessageBox.Show("请至少输入一个黑名单理由。");
+                return;
+            }
+
+            int userid = dbInstance.create(server, username, reason, info);
+            if (userid <= 0)
+            {
+                MessageBox.Show("添加失败。");
+                return;
+            }
+            else
+            {                
+                try
+                {
+                    PersonData[] result = dbInstance.queryID(userid);
+                    if (result.Length <= 0)
+                    {
+                        MessageBox.Show("添加失败。");
+                        return;
+                    }
+
+                    for(int i = 0; i < result.Length; i++)
+                    {
+                        DataGridViewRow row  = new DataGridViewRow();  
+                        row.CreateCells(this.dataGridView1);
+                        row.Cells[0].Value = result[i].id;
+                        row.Cells[1].Value = result[i].server;
+                        row.Cells[2].Value = result[i].username;
+                        row.Cells[3].Value = result[i].reason;
+                        row.Cells[4].Value = result[i].info;
+                        this.dataGridView1.Rows.Add(row);
+                    }
+                   
+                }
+                catch
+                {
+
+                }
+
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string server = this.comboBox1.Text;
+            string username = this.textBox1.Text;
+
+            if (server == "" && username == "")
+            {
+                MessageBox.Show("服务器和ID请至少输入一个。");
+                return;
+            }
+
+            this.dataGridView1.Rows.Clear();
+            PersonData[] result = dbInstance.queryUser(server, username);
+            if (result == null)
+            {
+                MessageBox.Show("无搜索结果");
+                return;
+            }
+
+            if (result.Length == 0)
+            {
+                MessageBox.Show("无搜索结果");
+                return;
+            }
+
+            
+            for (int i = 0; i < result.Length; i++)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row.CreateCells(this.dataGridView1);
+                row.Cells[0].Value = result[i].id;
+                row.Cells[1].Value = result[i].server;
+                row.Cells[2].Value = result[i].username;
+                row.Cells[3].Value = result[i].reason;                
+                row.Cells[4].Value = result[i].info;
+                this.dataGridView1.Rows.Add(row);
+            }
+
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            MouseEventArgs a = (MouseEventArgs)e;
+
+            if (a.Button == MouseButtons.Right)
+            {
+                if (dataGridView1.RowCount == 0)
+                    return;
+
+                if (MessageBox.Show("确定删除？", "", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.No)
+                    return;
+
+                foreach (var row in this.dataGridView1.SelectedRows)
+                {
+                    DataGridViewRow gridrow = (DataGridViewRow)row;
+                    int id = (int)gridrow.Cells[0].Value;
+                    if (0 <= dbInstance.delete(id))
+                    {
+                        this.dataGridView1.Rows.Remove(gridrow);
+                    }
+                }
+            }
+            else if(a.Button == MouseButtons.Right)
+            {
+
+            }
+            
+            
+        }
+
+    }
+
+
+    public class db
+    {
+        SQLiteConnection conn = null;
+        public db()
+        {
+            open();
+            createTBFACE();
+        }
+
+        public void open()
+        {
+            conn = new SQLiteConnection();
+            System.Data.SQLite.SQLiteConnectionStringBuilder connstr = new System.Data.SQLite.SQLiteConnectionStringBuilder();
+            connstr.DataSource = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "blacklist.db";
+            conn.ConnectionString = connstr.ToString();
+            conn.Open();
+        }
+
+        public void close()
+        {
+            //1. close the connection string
+            conn.Close();
+        }
+
+
+        private void createTBFACE()
+        {
+            string sql = "CREATE TABLE IF NOT EXISTS blacklist (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, server TEXT(128), name TEXT(128), reason TEXT(128), info TEXT(128))";
+            SQLiteCommand cmdCreateTable = new SQLiteCommand(sql, conn);
+            cmdCreateTable.ExecuteNonQuery();//如果表不存在，创建数据表  
+        }
+
+        public int create(string server, string username, string reason, string info)
+        {
+            if (ConnectionState.Open != conn.State)
+                return -1;
+
+            //2. do the transaction
+            SQLiteCommand cmd = new SQLiteCommand(conn);
+            cmd.CommandText = "insert into blacklist (server, name, reason, info) values(@server, @name, @reason, @info)";//设置带参SQL语句  
+            cmd.Parameters.AddRange(new[] {//添加参数  
+                new SQLiteParameter("@server", server),  
+                new SQLiteParameter("@name", username),  
+                new SQLiteParameter("@reason", reason),  
+                new SQLiteParameter("@info", info)
+            });
+
+            int id = 0;
+            cmd.ExecuteScalar();
+            cmd.CommandText = "SELECT last_insert_rowid()";
+            var resultSet = cmd.ExecuteScalar();
+            id = int.Parse(resultSet.ToString());
+            if (id > 0)
+                return id;
+            else
+                return -1;
+        }
+
+        public int delete(int id)
+        {
+            //1. get connection state
+            if (ConnectionState.Open != conn.State)
+                return -1;
+
+            //2. do the transaction
+            SQLiteCommand cmd = new SQLiteCommand(conn);
+            cmd.CommandText = "delete from blacklist where id = @id";
+            cmd.Parameters.AddRange(new[] {//添加参数  
+                    new SQLiteParameter("@id", id)
+                });
+
+            cmd.ExecuteNonQuery();
+            return 0;
+        }
+
+        public PersonData[] queryUser(string server, string name)
+        {
+            if (ConnectionState.Open != conn.State)
+                return null;
+
+            SQLiteCommand cmd = new SQLiteCommand(conn);
+            if (server == "" && name != "")
+                cmd.CommandText = "select * from blacklist where name = '" + name + "'";
+            else if (name == "" && server != "")
+                cmd.CommandText = "select * from blacklist where server = '" + server + "'";
+            else if (name != "" && server != "")
+                cmd.CommandText = "select * from blacklist where server = '" + server + "' and name = '" + name + "'";
+            else
+                return null;
+
+            System.Data.SQLite.SQLiteDataReader reader = cmd.ExecuteReader();
+            List<PersonData> personList = new List<PersonData>();
+
+            while (reader.Read())
+            {
+                PersonData person = new PersonData();
+                person.id = reader.GetInt16(0);
+                person.server = reader.GetString(1);
+                person.username = reader.GetString(2);
+                person.reason = reader.GetString(3);
+                person.info = reader.GetString(3);
+                personList.Add(person);
+            }
+
+            return personList.ToArray();
+        }
+
+        public PersonData[] queryReason(string reason)
+        {
+            if (ConnectionState.Open != conn.State)
+                return null;
+
+            SQLiteCommand cmd = new SQLiteCommand(conn);
+            cmd.CommandText = "select * from blacklist where reason = '" + reason + "'";
+            System.Data.SQLite.SQLiteDataReader reader = cmd.ExecuteReader();
+            List<PersonData> personList = new List<PersonData>();
+
+            while (reader.Read())
+            {
+                PersonData person = new PersonData();
+                person.id = reader.GetInt16(0);
+                person.server = reader.GetString(1);
+                person.username = reader.GetString(2);
+                person.reason = reader.GetString(3);
+                person.info = reader.GetString(3);
+                personList.Add(person);
+            }
+
+            return personList.ToArray();
+        }
+
+        public PersonData[] queryID(int id)
+        {
+            if (ConnectionState.Open != conn.State)
+                return null;
+
+            SQLiteCommand cmd = new SQLiteCommand(conn);
+            cmd.CommandText = "select * from blacklist where id = '" + id + "'";
+            System.Data.SQLite.SQLiteDataReader reader = cmd.ExecuteReader();
+            List<PersonData> personList = new List<PersonData>();
+            while (reader.Read())
+            {
+                PersonData person = new PersonData();
+                person.id = reader.GetInt16(0);
+                person.server = reader.GetString(1);
+                person.username = reader.GetString(2);
+                person.reason = reader.GetString(3);
+                person.info = reader.GetString(3);
+                personList.Add(person);
+            }
+
+            return personList.ToArray();
+        }
+    }
+
+    public struct PersonData
+    {
+        public int id;
+        public string server;
+        public string username;
+        public string reason;
+        public string info;
+        public PersonData(int a)
+        {
+            id = 0;
+            server = "";
+            username = "";
+            reason = "";
+            info = "";
+        }
+    }
+
+}
